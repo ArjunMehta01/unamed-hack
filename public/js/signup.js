0a865ca063fdda84188fab351b8261105f73b09f
arjun <arjun.mehta1001@gmail.com>
2021-05-29 15:06:05 -0600

    firebase changes

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,6 +1,7 @@
+(function() {
 
 
-var config = {
+  var config = {
     apiKey: "AIzaSyD08Vf5zlyun3msueD-npjX7DD41098KNE",
     authDomain: "hackon-98735.firebaseapp.com",
     databaseURL: "https://hackon-98735-default-rtdb.firebaseio.com",
@@ -10,9 +11,29 @@ var config = {
     appId: "1:221881769478:web:f420ccbd0de6296f596d87",
     measurementId: "G-SRKWNYYP10"
   }; 
-    firebase.initializeApp(config);
-    var database = firebase.database();
-    firebase.database().ref('testido').set({
-    username: "wack",
-    email: "email",
-});
+  firebase.initializeApp(config);
+
+
+  const txtEmail = document.getElementById('txtEmail');
+  const txtPassword = document.getElementById('txtPassword');
+  const signupButton = document.getElementById('signupButton');
+
+  signupButton.addEventListener('click', e => {
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+
+    const promise = auth.createUserWithEmailAndPassword(email, pass);
+    promise
+      .catch(e=>console.log(e.message));
+    
+    console.log("here");
+    // var database = firebase.database();
+    // console.log(email);
+    // firebase.database().ref("user/" + email).set({
+    // username: email,
+    // password: pass
+    // });
+  });
+}());
+
